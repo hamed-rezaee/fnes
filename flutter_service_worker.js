1a41aee94227c65bf367c363afeb2b4b1578234d
hamed-rezaee <hamed-rezaee@users.noreply.github.com>
2025-09-14 21:01:31 +0000

    deploy: 9fe7d563b5bbc662f689075eb3deee24ef478f0f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "1ef3ea3a0fec4569e5d531da25f34095",
 "assets/AssetManifest.bin": "4dda2d4391cf6047e33d5935aea4b896",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "main.dart.mjs": "73504e70ae0148344f7e914330602b8e",
-"flutter_bootstrap.js": "e93f3c83f3caee6a20709703e577dd4e",
+"flutter_bootstrap.js": "493b82ad0b5c0d15b4e044f56a6a8ea4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "36429a37f0c4aeb0295d89508e5a21ce",
 "/": "36429a37f0c4aeb0295d89508e5a21ce"};
